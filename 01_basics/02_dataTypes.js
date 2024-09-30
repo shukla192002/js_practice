971497fd37590d79d12c7f6b6746edb295cf0e37
SHAILENDRA SHUKLA <127722049+shukla192002@users.noreply.github.com>
2024-09-30 11:26:13 +0000

    datatypes coverage

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all JS code as newer version
+// alert("Hello") This can't be used directely as we use in browser console but not in nodeJS
+// alert(3+3)      it has a seprate syntax for it.
+console.log(3+3)
+
+let name="Shailenmdra" //string data type
+let age = 19 //int data type
+let isLoggedIn = false // Boolean data type
+
+//premitive data types
+// number => 2 to power 53 (range)
+//nigint
+//string =>" "
+//boolean = > true / false
+//null = > standalone value
+//undefined (it comes when we don't provide the value to that variable)
+// symbol( mostly used in react (used to define the uniqueness))
+
+
+//OBJECTS
+console.log(typeof "SHUKLA");
+
+console.log(typeof age);
+
+console.log(typeof undefined); //ouyput = undefined
+
+console.log(typeof null); // output = object
+
+
